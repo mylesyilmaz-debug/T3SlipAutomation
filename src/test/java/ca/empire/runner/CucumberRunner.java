760a1baeca7274247562bdfee1607a0b569a34de
Matt Denny <79858971+MattDenny@users.noreply.github.com>
2022-05-17 14:53:52 -0300

    Implement Runner

---
+++ src/test/java/ca/empire/runner/CucumberRunner.java
@@ -0,0 +1,13 @@
+package ca.empire.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/feature"},
+        glue = {"ca.empire"},
+        plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter"},
+        monochrome = true)
+public class CucumberRunner {}
